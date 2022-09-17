fca846ef3d739ce58c4b314ad3a58a046cbf74a8
Andrew Wooldridge <triptych@gmail.com>
2022-09-17 16:04:19 +0000

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-Lawrence's blog. Site at https://lawrencecchen.com
+# Andrew Wooldridge's Replit Blog (Triptych)
+This is a blog Repl for all my doings on Replit.
 
-Please feel free to make pull requests to the posts in the posts directory if you see any fixes that need to be made! (Or contribute translations etc)
 
-Blog posts, including the text and all images made by myself, are released freely to the world under the WTFPL.
+
+---
+Original code from 
+
+https://github.com/lawrencecchen/blog
+
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 import embeds from "astro-embed/integration";
+import svelte from '@astrojs/svelte';
+
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [embeds(), preact(), mdx(), tailwind()],
+  integrations: [embeds(), preact(), mdx(), tailwind(), svelte()],
   site: `http://astro.build`,
   vite: {
     ssr: {
